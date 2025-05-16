232a0ce1f81284bd70a33f44b95f0b67eb2e6724
Gabriel Henrique Alves Da Silva <infobighenrique@gmail.com>
2025-05-16 20:58:33 -0300

    parte 4 criação de seeds para categorias e ajustes nos models

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
@@ -10,8 +10,23 @@ class CategorySeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+        $categories = [
+            'Ficção',
+            'Não-ficção',
+            'Fantasia',
+            'Ciência',
+            'Biografia',
+            'História',
+            'Tecnologia',
+            'Arte',
+            'Culinária',
+            'Viagem'
+            ];
+
+            foreach ($categories as $category) {
+                 Category::create(['name' => $category]);
+            }
     }
 }
