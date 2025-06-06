61bcaf3d80adaac24e6df8bf886a3fc354727eeb
Gabriel Henrique <infobighenrique225@gmail.com>
2025-06-06 10:53:11 -0300

    feat: criação do index.blade.phpr

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,4 @@
 <?php
+use App\Http\Controllers\CategoryController;
 
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('categories', CategoryController::class);
