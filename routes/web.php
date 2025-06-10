f64c52855e1619987fd54516a79c293e158aaccc
Gabriel Henrique <infobighenrique225@gmail.com>
2025-06-09 22:41:21 -0300

    feat: configurar rotas e actions

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\AuthorsController;
-use App\Http\Controllers\PublishersController;
+use App\Http\Controllers\BookController;
 
-Route::resource('publishers', PublishersController::class);
-Route::resource('Author', AuthorsController::class);
-Route::resource('categories', CategoryController::class);
+// Rotas para criação de livros
+Route::get('/books/create-id-number', [BookController::class, 'createWithId'])->name('books.create.id');
+Route::post('/books/create-id-number', [BookController::class, 'storeWithId'])->name('books.store.id');
+
+Route::get('/books/create-select', [BookController::class, 'createWithSelect'])->name('books.create.select');
+Route::post('/books/create-select', [BookController::class, 'storeWithSelect'])->name('books.store.select');
+
+// Rotas RESTful para index, show, edit, update, delete (tem que ficar depois das rotas /books/create-id-number e /books/create-select)
+Route::resource('books', BookController::class)->except(['create', 'store']);
